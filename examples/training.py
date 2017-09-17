5cf59fef04fe774cda706f7e127708fbc98ea7ca
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-09-17 12:50:29 -0700

    example does batching

--- examples/training.py
+++ examples/training.py
@@ -1,18 +1,19 @@
 from __future__ import print_function
 
+import random
 import tensorflow as tf
 import live_feature_tf as lftf
 import my_live_features
 
 x = {}
-x['id'] = tf.constant(["Q1", "Q2"])
-x['baz'] = tf.constant([1.5, 2])
+x['id'] = tf.constant(["Q%04d" % i for i in xrange(100)])
+x['baz'] = tf.constant([random.random() for _ in xrange(100)])
 
 filenames = tf.placeholder(tf.string, shape=[None])
 dataset = tf.contrib.data.Dataset.from_tensor_slices(x)
 
 expander = lftf.Expander('id', my_live_features, cache_fn=None)
-dataset = expander.transform(dataset)
+dataset = expander.transform(dataset).batch(8)
 iterator = dataset.make_one_shot_iterator()
 next_elem = iterator.get_next()
 
