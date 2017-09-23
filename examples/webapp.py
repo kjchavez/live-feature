2428d37e8908da0a3788ec776e60db345b24d044
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-09-23 08:58:11 -0700

    flask app on the serving side

--- examples/webapp.py
+++ examples/webapp.py
@@ -1,38 +1,30 @@
-import live_feature as lf
+from flask import Flask
+from flask import jsonify
+from flask import request
 import logging
-import time
+import os
+import yaml
 
+import cloudml_client
 import my_live_features
+import live_feature as lf
 
-def test_batch():
-    x = {'id': ["Q001", "Q002"], 'baz': [1.5, 2.0]}
-    expander = lf.Expander(my_live_features, id_key='id')
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.debug = True
+
+expander = lf.Expander(my_live_features, id_key='id')
+
+@app.route("/")
+def home():
+    return "Running!"
+
+@app.route("/test")
+def test():
+    x = dict(request.args)
+    if 'id' not in x:
+        return "Must provide ID!"
     expander.apply(x)
-    print(x)
-    assert 'foo' in x
-    assert 'bar' in x
-    assert isinstance(x['foo'], list)
-    assert isinstance(x['bar'], list)
-
-def test_single():
-    x = {'id': "Q001", 'baz': 1.5}
-    expander = lf.Expander(my_live_features, id_key='id')
-    expander.apply(x)
-    print(x)
-    assert 'foo' in x
-    assert 'bar' in x
-
-def test_cache():
-    x = {'id': "Q001", 'baz': 1.5}
-    expander = lf.Expander(my_live_features, id_key='id', cache_fn=lf.cache.MemCache)
-    start = time.time()
-    for _ in xrange(10):
-        expander.apply(x)
-    end = time.time()
-    assert end - start < 1.0
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    test_batch()
-    test_single()
+    result = cloudml_client.predict_json("tf-livefeature", "test_prediction", [x])
+    return jsonify(result)
